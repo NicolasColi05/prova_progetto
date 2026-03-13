c84ec122f64b6934b42df4826425b2fe0b77cf7e
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2026-03-13 16:44:00 +0100

    funculòo

--- sample-gradle-project-master/src/main/java/it/unibo/sampleapp/RateAMovie.java
+++ sample-gradle-project-master/src/main/java/it/unibo/sampleapp/RateAMovie.java
@@ -25,46 +25,10 @@ public final class RateAMovie {
      * @param args a string with the movie/series name.
      */
     public static void main(final String[] args) {
-        if (OMDB_API_KEY == null || OMDB_API_KEY.isBlank()) {
-            LOGGER.error("Invalid OMDB API Key '{}', set a valid API Key as env variable OMDB_API_KEY", OMDB_API_KEY);
-            System.exit(1);
-        }
-        final String[] titles = args.length == 0 ? new String[] {DEFAULT_MOVIE} : args;
-        final OmdbApi omdb = new OmdbApi(OMDB_API_KEY);
-        Arrays.stream(titles)
-            .map(function(omdb::search)) // Exceptions become RuntimeExceptions
-            .flatMap(searchResult -> searchResult.getResults().stream()) // get all results and flatten
-            .map(function(movie -> // then get more details
-                new OmdbBuilder().setApiKey(OMDB_API_KEY).setPlotFull().setImdbId(movie.getImdbID()).build())
-            )
-            .map(function(omdb::getInfo))
-            .forEach(movie ->
-                LOGGER.info(
-                    """
-                    {}
-                    Directed by {}; {}; {}
-                    Written by {}
-                    Cast: {}
-                    {} {}
-                    IMDb {} (over {} votes), Rotten Tomatoes {}. {}
-                    {}
-                    IMDb handle: {}
-                    """,
-                    movie.getTitle(),
-                    movie.getDirector(),
-                    String.join(", ", movie.getCountries()),
-                    movie.getYear(),
-                    movie.getWriter(),
-                    movie.getActors(),
-                    movie.getRuntime(),
-                    movie.getGenre(),
-                    movie.getImdbRating(),
-                    movie.getImdbVotes(),
-                    movie.getTomatoRating(),
-                    movie.getAwards(),
-                    movie.getPlot(),
-                    movie.getImdbID()
-                )
-            );
+       System.out.println("ciao");
+       int i = 0;
+       if(i == 0) {
+        System.out.println("gay");
+       }
     }
 }
